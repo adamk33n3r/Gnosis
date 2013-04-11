2b418fab859578e1b7b2e733e739c9af72ed9089
Adam Keenan <adamk33n3r@gmail.com>
2013-04-11 17:47:46 -0400

    Added Games activity

--- src/org/waldk33n3r/android/gnosis/HomeScreen.java
+++ src/org/waldk33n3r/android/gnosis/HomeScreen.java
@@ -1,8 +1,11 @@
 package org.waldk33n3r.android.gnosis;
 
+import org.waldk33n3r.android.gnosis.games.Games;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Menu;
@@ -11,13 +14,14 @@ import android.view.MenuItem;
 
 public class HomeScreen extends Activity implements OnClickListener {
 	@Override
-	public void onCreate(Bundle savedInstance){
+	public void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
 		setContentView(R.layout.home_screen);
-		
-		//Add event listener to buttons here
+
+		// Add event listener to buttons here
 		View quizButton = this.findViewById(R.id.quiz_button);
 		quizButton.setOnClickListener(this);
+		this.findViewById(R.id.game_button).setOnClickListener(this);
 		View bibleButton = this.findViewById(R.id.bible_button);
 		bibleButton.setOnClickListener(this);
 		View statsButton = this.findViewById(R.id.stats_button);
@@ -25,25 +29,33 @@ public class HomeScreen extends Activity implements OnClickListener {
 		View aboutButton = this.findViewById(R.id.about_button);
 		aboutButton.setOnClickListener(this);
 	}
-	
+
 	@Override
-	public void onClick(View v){
-		switch(v.getId()){
-		case R.id.quiz_button:
-			startActivity(new Intent(this, QuizMenu.class));
-			break;
-		case R.id.bible_button:
-			startActivity(new Intent(this, Bible.class));
-			break;
-		case R.id.stats_button:
-			startActivity(new Intent(this, Statistics.class));
-			break;
-		case R.id.about_button:
-			startActivity(new Intent(this, About.class));
-			break;
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.quiz_button:
+				startActivity(new Intent(this, QuizMenu.class));
+				break;
+			case R.id.game_button:
+				startActivity(new Intent(this, Games.class));
+				break;
+			case R.id.bible_button:
+				startActivity(new Intent(this, Bible.class));
+				break;
+			case R.id.stats_button:
+				startActivity(new Intent(this, Statistics.class));
+				break;
+			case R.id.about_button:
+				startActivity(new Intent(this, About.class));
+				break;
+			default:
+				Log.e("Activity", "Loading new activity");
+				startActivity(new Intent(this, About.class));
+				Log.e("Activity", "Ending new activity");
+				break;
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
@@ -51,89 +63,52 @@ public class HomeScreen extends Activity implements OnClickListener {
 		inflater.inflate(R.menu.menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.settings:
 				startActivity(new Intent(this, Settings.class));
-			return true;
+				return true;
 		}
-	return false;
+		return false;
 	}
 }
 
 /*
- * @Override
-	public void onCreate(Bundle savedInstance){
-		super.onCreate(savedInstance);
-		setContentView(R.layout.home_screen);
-		
-		//Add event listener to buttons here
-		View quizButton = this.findViewById(R.id.quiz_button);
-		quizButton.setOnClickListener(this);
-		View bibleButton = this.findViewById(R.id.bible_button);
-		bibleButton.setOnClickListener(this);
-		View statsButton = this.findViewById(R.id.stats_button);
-		statsButton.setOnClickListener(this);
-		View aboutButton = this.findViewById(R.id.about_button);
-		aboutButton.setOnClickListener(this);
-	}
-	
-	@Override
-	public void onClick(View v){
-		switch(v.getId()){
-		case R.id.quiz_button:
-			startActivity(new Intent(this, QuizMenu.class));
-			break;
-		case R.id.bible_button:
-			startActivity(new Intent(this, Bible.class));
-			break;
-		case R.id.stats_button:
-			startActivity(new Intent(this, Statistics.class));
-			break;
-		case R.id.about_button:
-			startActivity(new Intent(this, About.class));
-			break;
-		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu, menu);
-		return true;
-	}
+ * @Override public void onCreate(Bundle savedInstance){
+ * super.onCreate(savedInstance); setContentView(R.layout.home_screen);
+ * 
+ * //Add event listener to buttons here View quizButton =
+ * this.findViewById(R.id.quiz_button); quizButton.setOnClickListener(this);
+ * View bibleButton = this.findViewById(R.id.bible_button);
+ * bibleButton.setOnClickListener(this); View statsButton =
+ * this.findViewById(R.id.stats_button); statsButton.setOnClickListener(this);
+ * View aboutButton = this.findViewById(R.id.about_button);
+ * aboutButton.setOnClickListener(this); }
+ * 
+ * @Override public void onClick(View v){ switch(v.getId()){ case
+ * R.id.quiz_button: startActivity(new Intent(this, QuizMenu.class)); break;
+ * case R.id.bible_button: startActivity(new Intent(this, Bible.class)); break;
+ * case R.id.stats_button: startActivity(new Intent(this, Statistics.class));
+ * break; case R.id.about_button: startActivity(new Intent(this, About.class));
+ * break; } }
+ * 
+ * @Override public boolean onCreateOptionsMenu(Menu menu) {
+ * super.onCreateOptionsMenu(menu); MenuInflater inflater = getMenuInflater();
+ * inflater.inflate(R.menu.menu, menu); return true; }
  * 
  * 
- * <Button
-      android:id="@+id/quiz_button"
-      android:layout_width="fill_parent"
-      android:layout_height="wrap_content"
-      android:text="@string/quiz_label"
-   	  android:gravity="center"
-   	  android:textSize="25sp" />
-   <Button
-      android:id="@+id/bible_button"
-      android:layout_width="fill_parent"
-      android:layout_height="wrap_content"
-      android:text="@string/bible_label"
-      android:textSize="25sp"
-      android:gravity="center" />
-   <Button
-      android:id="@+id/stats_button"
-      android:layout_width="fill_parent"
-      android:layout_height="wrap_content"
-      android:text="@string/stats_label"
-      android:textSize="25sp"
-      android:gravity="center" />
-   <Button
-      android:id="@+id/about_button"
-      android:layout_width="fill_parent"
-      android:layout_height="wrap_content"
-      android:text="@string/about_label"
-      android:textSize="25sp"
-      android:gravity="center" />
- * 
+ * <Button android:id="@+id/quiz_button" android:layout_width="fill_parent"
+ * android:layout_height="wrap_content" android:text="@string/quiz_label"
+ * android:gravity="center" android:textSize="25sp" /> <Button
+ * android:id="@+id/bible_button" android:layout_width="fill_parent"
+ * android:layout_height="wrap_content" android:text="@string/bible_label"
+ * android:textSize="25sp" android:gravity="center" /> <Button
+ * android:id="@+id/stats_button" android:layout_width="fill_parent"
+ * android:layout_height="wrap_content" android:text="@string/stats_label"
+ * android:textSize="25sp" android:gravity="center" /> <Button
+ * android:id="@+id/about_button" android:layout_width="fill_parent"
+ * android:layout_height="wrap_content" android:text="@string/about_label"
+ * android:textSize="25sp" android:gravity="center" />
  */
---
+++ src/org/waldk33n3r/android/gnosis/games/Games.java
@@ -0,0 +1,36 @@
+package org.waldk33n3r.android.gnosis.games;
+
+import org.waldk33n3r.android.gnosis.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class Games extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.games);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.games, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
